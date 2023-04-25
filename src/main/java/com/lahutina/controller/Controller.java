f414fc4b1e341341663c53b16f73757fbdd8bfe2
Oksana Lahutina <oksankalahutina@gmail.com>
2023-04-25 22:52:37 +0300

    Update DateService, refactor Controller

--- src/main/java/com/lahutina/controller/Controller.java
+++ src/main/java/com/lahutina/controller/Controller.java
@@ -2,28 +2,26 @@ package com.lahutina.controller;
 
 import com.lahutina.model.Date;
 import com.lahutina.service.DateService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/year-pass")
 public class Controller {
 
     private final DateService dateService;
 
-    @Autowired
     public Controller(DateService dateService) {
         this.dateService = dateService;
     }
 
-    @GetMapping("/year-pass")
+    @GetMapping
     public ResponseEntity<Double> getPercentageFromCurrentDay() {
         return new ResponseEntity<>(dateService.getDaysFromCurrentDay(), HttpStatus.OK);
     }
 
-    @PostMapping("/year-pass")
+    @PostMapping
     public ResponseEntity<Double> getPercentageDaysFromSpecificDay(@RequestBody Date date) {
         double percDaysPassed = dateService.getDaysFromSpecificDay(date);
 
--- src/main/java/com/lahutina/service/DateService.java
+++ src/main/java/com/lahutina/service/DateService.java
@@ -1,38 +1,9 @@
 package com.lahutina.service;
 
 import com.lahutina.model.Date;
-import org.springframework.stereotype.Service;
 
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-import java.time.temporal.ChronoUnit;
+public interface DateService {
 
-@Service
-public class DateService {
-    public double getDaysFromCurrentDay() {
-        LocalDate currentDate = LocalDate.now();
-        return calculateYearPassPercentage(currentDate);
-    }
-
-    public double getDaysFromSpecificDay(Date date)
-    {
-        if (date == null || date.getDate() == null) {
-            return -1;
-        }
-
-        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd.MM.yyyy");
-        LocalDate inputDate = LocalDate.parse(date.getDate(), dtf);
-
-        return calculateYearPassPercentage(inputDate);
-    }
-
-    private double calculateYearPassPercentage(LocalDate date) {
-        LocalDate startDate = LocalDate.of(date.getYear(), 1, 1);
-        LocalDate endDate = LocalDate.of(date.getYear(), 12, 31);
-
-        double totalDaysInYear = ChronoUnit.DAYS.between(startDate, endDate) + 1;
-        double daysPassed = ChronoUnit.DAYS.between(startDate, date) + 1;
-
-        return daysPassed / totalDaysInYear * 100;
-    }
+    double getDaysFromCurrentDay();
+    double getDaysFromSpecificDay(Date date);
 }
